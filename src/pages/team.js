72bde53f6456023955ad6be340dfb2f509720d13
varun <varunvaru27101999@gmail.com>
2021-04-19 19:49:32 +0530

    edited teams page

--- src/pages/team.js
+++ src/pages/team.js
@@ -8,32 +8,43 @@ import VarunImage from "../images/varun.jpg"
 import { slugify } from "../utils/utilityFunctions"
 
 const TeamPage = () => (
-  <Layout pageTitle="Our Team">
+  <Layout>
     <Seo title="Team" keywords={[`gatsby`, `application`, `react`]} />
-    <Row className="md-4">
-      <div className="col-md-3">
-        <img
-          src={VarunImage}
-          style={{ maxWidth: "100%" }}
-          alt="Varun author profile"
-        />
+    <div className="teams-container">
+      <div className="teams">
+        <h2>Meet the Team.</h2>
+        <p>
+          The only way to create great things every day is with great people who
+          truly enjoy their work, below are the authors and editors who are
+          responsible of the success of this site.
+        </p>
+        <h3>Our folk.</h3>
+        <Row className="md-4">
+          <div className="col-md-3">
+            <img
+              src={VarunImage}
+              style={{ maxWidth: "100%" }}
+              alt="Varun author profile"
+            />
+          </div>
+          <div className="col-md-8">
+            <Card style={{ minHeight: "100%" }}>
+              <CardBody>
+                <CardTitle>{authors[0].name}</CardTitle>
+                <CardText>{authors[0].bio}</CardText>
+                <Button
+                  className="text-uppercase"
+                  color="primary"
+                  href={`/author/${slugify(authors[0].name)}`}
+                >
+                  View Posts
+                </Button>
+              </CardBody>
+            </Card>
+          </div>
+        </Row>
       </div>
-      <div className="col-md-8">
-        <Card style={{ minHeight: "100%" }}>
-          <CardBody>
-            <CardTitle>{authors[0].name}</CardTitle>
-            <CardText>{authors[0].bio}</CardText>
-            <Button
-              className="text-uppercase"
-              color="primary"
-              href={`/author/${slugify(authors[0].name)}`}
-            >
-              View Posts
-            </Button>
-          </CardBody>
-        </Card>
-      </div>
-    </Row>
+    </div>
   </Layout>
 )
 
