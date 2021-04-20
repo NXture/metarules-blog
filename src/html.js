cb2eaad4083f4292d35429df6975952a9f27cda2
varun <varunvaru27101999@gmail.com>
2021-04-20 11:57:25 +0530

    added adsense script tag

--- src/html.js
+++ src/html.js
@@ -11,7 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        
+        <script data-ad-client="ca-pub-7004283638114501" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
